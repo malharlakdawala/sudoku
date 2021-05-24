fa9d9e45fb277ce0f994ca9afebb834f68cf3814
malhar <malharlakdawala@gmail.com>
2021-05-24 09:52:46 +0530

    second

--- Solution.js
+++ Solution.js
@@ -1,4 +1,4 @@
-let grid = [
+let input_sudoku = [
     [1, 0, 5, 0, 7, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -10,19 +10,30 @@ let grid = [
     [0, 0, 0, 0, 0, 0, 0, 0, 0]
 ]
 
-let variable = 1
 
-for(i=0; i<9; i++)
-{
-if (grid[0][i] == 0) {
+//Declaring functions to get rows, columns, squares, checkerboard, print board,
 
-grid[0][i] = variable;
-variable++;
 
-//https://github.com/davelms/medium-articles/blob/master/sudoku-solver/sudoku.js
-//https://javascript.plainenglish.io/solve-a-sudoku-using-javascript-de456e8c34a5
-}
+function horizontal(grid, h){
+    return grid[h];
 }
 
+function vertical(grid,v){
 
-console.log(grid[0]);
+    var col = []
+    for(i=0;i<9;i++){
+        col[i] = grid[i][0]
+    }
+    return col;
+}
+
+console.log(vertical(input_sudoku,0))
+
+
+function squareblock(grid,r,c){
+    var square_array []
+
+}//pending
+
+
+
